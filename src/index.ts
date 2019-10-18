05e016eb4c4f559dac0e04e31593de06b2990d88
Kenshi Shiode <atemu1212@yahoo.co.jp>
2019-10-18 23:38:18 +0900

    lib

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-const test: string = 'a';
-test + 'a';
+import Immutable from 'immutable';
+import Serialize, { Refs } from 'remotedev-serialize';
+import ReduxPersist from 'redux-persist';
+
+interface TransformConfig {
+  whitelist?: Array<string>;
+  blacklist?: Array<string>;
+}
+
+export default function(records: Array<Refs>, config: TransformConfig) {
+  const { stringify, parse } = Serialize.immutable(Immutable, records);
+  return ReduxPersist.createTransform(stringify, parse, config);
+}
